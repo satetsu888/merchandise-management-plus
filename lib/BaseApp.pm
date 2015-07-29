b67cf4d0ff2711c1a94ed76579839be186ac29bc
shotaro.akiyama <satetsu888@gmail.com>
2015-07-29 23:55:06 +0900

    Add callback page

--- lib/BaseApp.pm
+++ lib/BaseApp.pm
@@ -13,6 +13,7 @@ sub startup {
 
   # Normal route to controller
   $r->get('/')->to('example#welcome');
+  $r->get('/callback')->to('example#callback');
 }
 
 1;
--- lib/BaseApp/Controller/Example.pm
+++ lib/BaseApp/Controller/Example.pm
@@ -1,12 +1,39 @@
 package BaseApp::Controller::Example;
 use Mojo::Base 'Mojolicious::Controller';
 
+use URI;
+use URI::QueryParam;
+
+use constant {
+    AUTH_URI_BASE => 'https://api.thebase.in/1/oauth/authorize',
+};
+
 # This action will render a template
 sub welcome {
   my $self = shift;
 
+
+  my $auth_uri = URI->new(AUTH_URI_BASE);
+  $auth_uri->query_form({
+    response_type => 'code',
+    client_id     => '4823f3464dc6ab2a9c354dcbb8874f4d',
+    redirect_uri  => 'http://satetsu888.com/base_app/callback',
+    scope         => 'read_items write_items',
+    state         => 'state',
+  });
+
+  $self->redirect_to($auth_uri->as_string);
+
+}
+
+sub callback {
+  my $self = shift;
+
   # Render template "example/welcome.html.ep" with message
-  $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
+  $self->render(
+      code => $self->param('code'),
+      msg  => 'コールバックページ',
+  );
 }
 
 1;
