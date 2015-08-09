06b13635da745df69e24b0031e43bc0986e70796
shotaro.akiyama <satetsu888@gmail.com>
2015-08-09 09:12:49 +0900

    Add variation and update button

--- public/js/items.js
+++ public/js/items.js
@@ -6,23 +6,44 @@ var sortableList = function(){
     $("#itemList").sortable();
 }
 
+var getItems = function(vm){
+    $.jsonRPC.request('getItems',{
+        params: {},
+        success: function(res){
+            vm.items = res.result.items.concat();
+            Vue.nextTick( sortableList );
+        },
+        error: function(res){
+            console.log(res);
+        }
+    });
+}
+
+var setItems = function(vm){
+    $.jsonRPC.request('setItems',{
+        params: { data: vm.items },
+        success: function(res){
+            console.log(res);
+        },
+        error: function(res){
+            console.log(res);
+        }
+    });
+}
+
 var vm = new Vue({
-    el: '#itemList',
+    el: '#itemListWrapper',
     data: {
         items : [],
     },
     ready: function(){
         var self = this;
-        $.jsonRPC.request('getItems',{
-            params: {},
-            success: function(res){
-                self.items = res.result.items.concat();
-                Vue.nextTick( sortableList );
-            },
-            error: function(res){
-                console.log(res);
-            }
-        });
+        getItems(self);
+    },
+    methods: {
+        updateItems: function(e){
+            setItems(e.targetVM);
+        },
     }
 });
 
